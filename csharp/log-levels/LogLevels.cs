539a9d8d09ad8c35cfedc732b933ef4e994b3cf4
Luigi Marino <emails@luigi-marino.com>
2025-07-22 07:57:44 +0100

    C# - Log Levels - Completed

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -1,17 +1,12 @@
 static class LogLine
 {
-    public static string Message(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
+    public static string Message(string logLine) => logLine.Split(":")[1].Trim();
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        string errorLevel = logLine.Split(":")[0].Trim();
+        return string.Join("", errorLevel.Split(':', '[', ']')).ToLower();
     }
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
 }
