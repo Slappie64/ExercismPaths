d804ae6a85d04d8b4bb99f1ed166b7cdc2f552cd
Luigi Marino <emails@luigi-marino.com>
2025-07-23 06:15:58 +0100

    Added daily challenges for CS, Python & Powershell

--- csharp/hello-world/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08cd9d85703cfaa8c14befbae19fae3bb749223b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb893c8f7412fb38347652c1fbabd5ff45824c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
