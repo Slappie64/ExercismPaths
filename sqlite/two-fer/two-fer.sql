3d99415311d5562e4478834f64bccbfa8254430c
Luigi Marino <emails@luigi-marino.com>
2025-07-25 16:40:12 +0100

    SQLite - Twofer - Complete

--- sqlite/two-fer/two-fer.sql
+++ sqlite/two-fer/two-fer.sql
@@ -1,2 +1,7 @@
 -- Schema: CREATE TABLE "twofer" ("input" TEXT, "response" TEXT);
 -- Task: update the twofer table and set the response based on the input.
+UPDATE twofer
+SEt response = CASE 
+    WHEN input = "" THEN  "One for you, one for me."
+    ELSE  "One for " || input || ", one for me."
+END;
