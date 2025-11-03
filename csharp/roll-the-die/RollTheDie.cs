464304eb1d1532fa0be3d03e34cbe6a8ffe7b86b
Luigi Marino <emails@luigi-marino.com>
2025-11-03 10:09:27 +0000

    Removed Duplicate Files

--- csharp/roll-the-die/RollTheDie.cs
+++
@@ -1,6 +0,0 @@
-public class Player
-{
-    public int RollDie() => new Random().Next(1, 19);
-
-    public double GenerateSpellStrength() => new Random().NextDouble() * 100;
-}
--- csharp/tim-from-marketing/TimFromMarketing.cs
+++
@@ -1,5 +0,0 @@
-static class Badge
-{
-    public static string Print(int? id, string name, string? department) => $"{(id == null ? "" : $"[{id}] - ")}{name} - {department?.ToUpper() ?? "OWNER"}";
-    
-}
